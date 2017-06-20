4622ae776868c52324d6486358030cb1b5e8ff71
nhatsovico <nhatsovico@gmail.com>
2017-06-20 10:30:39 +0700

    lesson6

--- dist/lesson6.js
+++ dist/lesson6.js
@@ -1,4 +1,16 @@
 "use strict";
-var a1;
-var number = 10;
+var deck = {
+    suits: ["hearts", "spades", "clubs", "diamonds"],
+    cards: Array(52),
+    createCardPicker: function () {
+        // NOTE: the line below is now an arrow function, allowing us to capture 'this' right here
+        var pickedCard = Math.floor(Math.random() * 52);
+        var pickedSuit = Math.floor(pickedCard / 13);
+        return { suit: this.suits[pickedSuit], card: pickedCard % 13 };
+    }
+};
+var cardPicker = deck.createCardPicker();
+//let pickedCard = cardPicker();
+//alert("card: " + pickedCard.card + " of " + pickedCard.suit);
+alert("card: " + cardPicker.card + " of " + cardPicker.suit);
 //# sourceMappingURL=lesson6.js.map
--- lesson6.ts
+++ lesson6.ts
@@ -1,4 +1,18 @@
-let a1;
-const number = 10;
 
-let go = ()=> console.log('a'); console.log('b');
+let deck = {
+    suits: ["hearts", "spades", "clubs", "diamonds"],
+    cards: Array(52),
+    createCardPicker: function() {
+        // NOTE: the line below is now an arrow function, allowing us to capture 'this' right here
+        let pickedCard = Math.floor(Math.random() * 52);
+            let pickedSuit = Math.floor(pickedCard / 13);
+
+            return {suit: this.suits[pickedSuit], card: pickedCard % 13};
+    }
+}
+
+let cardPicker = deck.createCardPicker();
+//let pickedCard = cardPicker();
+
+//alert("card: " + pickedCard.card + " of " + pickedCard.suit);
+alert("card: " + cardPicker.card + " of " + cardPicker.suit);
